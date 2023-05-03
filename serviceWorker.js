fdf8c8aebeda7ff78966f9a9c58f5a352e81110d
ElBitan <93447663+ElBitan@users.noreply.github.com>
2023-05-04 00:59:07 +0200

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,39 @@
+var cacheName = 'HAWA-SHERT';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+   ...
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheKeeplist = ['cacheName'];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
